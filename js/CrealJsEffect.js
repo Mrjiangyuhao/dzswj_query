b602f586119df42d6176488903567afd4d74ed38
Mrjiangyuhao <690392431@qq.com>
2019-03-03 22:39:07 +0800

    1:修修复进度条BUG  2：添加检测恶意关键字

--- js/CrealJsEffect.js
+++ js/CrealJsEffect.js
@@ -2,28 +2,32 @@
  * @Author: Pierre
  * @Date: 2019-02-09 16:10:43
  * @LastEditors: Pierre
- * @LastEditTime: 2019-02-23 17:36:52
- * @Description:  CssEffects
+ * @LastEditTime: 2019-03-03 22:10:54
+ * @Description:  Progress bar
  */
 var lan = 0;
 function loading_progress(smallNumber,maxNumber){
      let bar = document.getElementById("bar");
      //进度条
      if( bar === null ){
+          //动态生成节点
           var oDiv_bar = document.createElement("div");
           oDiv_bar.setAttribute('id','bar');
-          oDiv_bar.innerHTML = `<div id='bar_title'>&nbsp&nbsploadding...<button id='bar_btn'>X</button></div> \
+          oDiv_bar.innerHTML = `<div id='bar_title'>&nbsp&nbsploadding... \
           <progress value='${smallNumber}'  max='${maxNumber}'></progress><span id='loadding'>0%</span>`;
           document.body.appendChild(oDiv_bar);
-          // loading_progress(smallNumber,maxNumber)   //测试
+
+      // loading_progress(smallNumber,maxNumber)   //测试
      }else{
           var loadding = document.getElementById("loadding");
           var bar_progress = bar.getElementsByTagName("progress")[0];
           var loadding_bar = document.getElementById("bar");
-          //....    此处写删除display属性
           bar_progress.setAttribute('value',smallNumber)
           lan =  (smallNumber / maxNumber) * 100;   //加 0 - 就为负数不加为正
           loadding.innerText = Math.floor(lan) + '%';
+          if(  Math.floor(lan) === 100 ){
+               unload_progress()     
+          }
           return
      }
 
@@ -32,5 +36,6 @@ function loading_progress(smallNumber,maxNumber){
 function unload_progress(){
      var unload_bar = document.getElementById("bar");
      unload_bar.style.cssText = 'display: none';
+     lan = 0;
 }
-// loading_progress(500,500)  //测试
+ //loading_progress(500,500)  //测试
--- js/load.js
+++ js/load.js
@@ -2,45 +2,39 @@
  * @Author: Pierre
  * @Date: 2019-01-03 19:31:35
  * @LastEditors: Pierre
- * @LastEditTime: 2019-02-23 17:29:51
+ * @LastEditTime: 2019-03-03 22:33:56
  * @Description: 入口
  */
+// 首次获取验证码图片
+window.onload = function() {
+  alert("批量导出暂时只支持100条数据的查询导出，多的会报错。建议分开查询");
+  let Vimg = document.getElementsByTagName("img");
+  let Tx_sr = document.getElementById("Tx_sr");
+  let Bt_tj = document.getElementById("Bt_tj");
+  let Bt_TX = document.getElementById("test_1");
+  let Bt_TX1 = document.getElementById("test_2");
+  let upload = document.getElementById("upload_tj");
+  let upload_file = document.getElementById("upload_file");
+  let url = null;
+  let rABS = true; //true为二进制, false 为ArrayBuffer
+  Vimg[0].src = "/web-tycx/gzrk/builderCaptcha.do?t=" + new Date().getTime();
 
-    // 首次获取验证码图片
-    window.onload = function () {
-        let Vimg = document.getElementsByTagName('img');
-        let Tx_sr = document.getElementById('Tx_sr');
-        let Bt_tj = document.getElementById('Bt_tj');
-        let Bt_TX = document.getElementById('test_1');
-        let Bt_TX1 = document.getElementById('test_2');
-        let upload = document.getElementById('upload_tj');
-        let upload_file = document.getElementById('upload_file');
-        let unload_progress_bar = document.getElementById('bar');
-        let url = null;
-        let rABS = true;    //true为二进制, false 为ArrayBuffer
-        Vimg[0].src = '/web-tycx/gzrk/builderCaptcha.do?t=' + new Date().getTime();
-     
-        //读取xls文件
-        upload_file.addEventListener('change',function(e){
-            xls(upload_file,rABS,e)   //读Excle内容
-        })
+  //读取xls文件
+  upload_file.addEventListener("change", function(e) {
+    xls(upload_file, rABS, e); //读Excle内容
+  });
 
-        //单独查询
-        Bt_tj.addEventListener('click',function(){
-            //截取t后面时间值
-            url = Vimg[0].src;
-            var time_id =  getString(url, 1) 
-            Distion(time_id,Tx_sr.value,'only')
-        })
-
-        //退出进度条
-        unload_progress_bar.addEventListener('click',unload_progress)
+  //单独查询
+  Bt_tj.addEventListener("click", function() {
+    console.log(isNaN(Tx_sr.value))
+    if (Tx_sr.value != "" && isNaN(Tx_sr.value)) {
+      //截取t后面时间值
+      url = Vimg[0].src;
+      var time_id = getString(url, 1);
+      Distion(time_id, Tx_sr.value, "only");
+    }else{
+        alert('请输入公司名称！')
+        return
     }
-
-
-
-
-
-
-
-
+  });
+};
